143027273e9cd54f6c1344bb886033777004169b
Felix Diaz <pixcompu@outlook.com>
2016-05-26 18:53:07 -0500

    Fixing index

--- sections/principal.php
+++ sections/principal.php
@@ -24,25 +24,7 @@
 </div>
 <img src="../resources/images/icons/space-bird.png" id="bird" alt="">
 <div id="principal">
-    <div id="header">
-        <div id="brand">
-            <img src="../resources/images/tab/tab_icon.png" alt="">
-            <p>GeekStore</p>
-        </div>
-        <div id="session">
-            <ul id="session-options">
-                <li><a class="login-item" href="register.php">Reg&iacute;strate</a></li>
-                <li><a class="login-item" href="login.php">Inicia Sesi&oacute;n</a></li>
-            </ul>
-        </div>
-        <div id="navbar">
-            <ul id="navbar-options">
-                <li><a class="navbar-item" href="catalog.php">Productos</a></li>
-                <li><a class="navbar-item" href="cart.php">Mi Carrito</a></li>
-                <li><a class="navbar-item" href="contact.php">Contacto</a></li>
-            </ul>
-        </div>
-    </div>
+    <?php require_once('navbar.php'); ?>
     <div id="content">
         <div id="products">
             <h1>Episodio Geekstore</h1>
