65171228ff6308372c9584ca0fcf14aaea1e729b
Felix Diaz <pixcompu@outlook.com>
2016-05-27 15:37:24 -0500

    Fixing register product form and email

--- server/action/mail/send.php
+++ server/action/mail/send.php
@@ -1,14 +1,21 @@
 <?php
 require("sendgrid-php/sendgrid-php.php");
+require_once('../../autoloader.php');
+
+$destinatary = $_POST['destinatary'];
+$message = $_POST['message'];
+
 $sendgrid = new SendGrid('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');
 $email = new SendGrid\Email();
 $email
+    ->addTo('luis.burgos.1995@gmail.com')
     ->addTo('pixcompu@outlook.com')
-    ->setFrom('geekstore@pixcompu.esy.es')
-    ->setSubject('Subject goes here')
-    ->setText('Hello World!')
-    ->setHtml('<strong>Hello World!</strong>');
+    ->addTo('mdoming@uady.mx')
+    ->setFrom($destinatary)
+    ->setSubject('Nuevo contacto de GeekStore2')
+    ->setText($message)
+    ->setHtml('<strong>'.$message.'</strong>');
 $sendgrid->send($email);
 
-respondWithSuccess('Email enviado');
+respondWithSuccess('Tu opinion es muy valiosa, ¡Gracias!');
 
