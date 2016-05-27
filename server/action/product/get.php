a5dfa307b2cf65e218512c98190e14a6a998f07a
LuisBurgos <luisarturo.burgos@gmail.com>
2016-05-27 08:03:11 -0500

    Done search box by letter entered

--- server/action/product/get.php
+++ server/action/product/get.php
@@ -2,9 +2,18 @@
 require_once('../../autoloader.php');
 
 $product = new Product();
-try{
-    $products = $product->all();
-    respondWithSuccess($products);
-}catch(Exception $e){
-    respondWithError($e->getMessage());
+if (isset($_POST["search"]) && !empty($_POST["search"])) {
+    try{
+        $products = $product->findAll($_POST["search"]);
+        respondWithSuccess($products);
+    }catch(Exception $e){
+        respondWithError($e->getMessage());
+    }
+} else {
+    try{
+        $products = $product->all();
+        respondWithSuccess($products);
+    }catch(Exception $e){
+        respondWithError($e->getMessage());
+    }
 }
